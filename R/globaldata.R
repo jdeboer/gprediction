dd6b7ab7eb18c0b1fea106e89fabad83a6f8a6f1
unknown <johann.deboer@mac-0287.open.edu.au>
2013-09-09 12:32:58 +1000

    Clean up package structure

---
+++ R/globaldata.R
@@ -0,0 +1,1 @@
+base_url <- "https://www.googleapis.com/prediction/v1.6"
---
+++ R/gprediction-package.R
@@ -0,0 +1,17 @@
+#' gprediction
+#' 
+#' Google Prediction API client for R
+#' 
+#' A Google Prediction API client for R supporting OAuth2.0
+#' 
+#' @references Google Predication API developer reference
+#' \url{https://developers.google.com/prediction/docs/getting-started}
+#' 
+#' @keywords package
+#' @import httr
+#' @docType package
+#' @name ganalytics
+#' @aliases ganalytics ganalytics-package
+NULL
+
+#' @include globaldata.R
