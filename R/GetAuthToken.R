5a6923c8bfded016dced7082b24dd84ce2a42d43
unknown <johann.deboer@mac-0287.open.edu.au>
2013-09-09 12:33:34 +1000

    Add documentation

--- R/GetAuthToken.R
+++ R/GetAuthToken.R
@@ -1,4 +1,15 @@
-GetAuthToken <- function(file, appname, key = NULL, secret = NULL) {
+#' GetAuthToken
+#' Get an OAuth2.0 authentication token for accessing the Google Prediction API
+#' @param appname name of the Google APIs project for authentication
+#' @param file optional path and filename where to save the OAuth2.0 token for
+#'   reuse
+#' @param key optional OAuth key to use or NULL to use the key in the
+#'   environment variables for appname
+#' @param secret optional OAuth secret to use or NULL to use the secret in the
+#'   environment variables for appname
+#' @return an OAuth2.0 reference class object to use for httr requests
+#' @export
+GetAuthToken <- function(appname, file = NULL, key = NULL, secret = NULL) {
   auth <- new_oauth(
     base_url = "https://accounts.google.com/o/oauth2",
     authorize = "auth",
--- R/GpApiRequest.R
+++ R/GpApiRequest.R
@@ -1,3 +1,25 @@
+#' GpApiRequest
+#' 
+#' Perform an Google Prediction API request and return the results
+#' 
+#' Construct and perform an HTTP request to the Google Prediction API. Transform
+#' the JSON output into an R list.
+#' 
+#' @param req_type Character string specifying the type of http request to
+#'   perform: "GET", "POST", "PUT", or "DELETE".
+#' @param oauth The httr OAuth2.0 token reference class object to use for
+#'   authentication.
+#' @param project Character string. The name of the Google APIs project that the
+#'   oauth token belong to.
+#' @param body_list optional An R list object containing the name value pairs
+#'   for the body of the request, where applicable. This list is coverted into
+#'   JSON by the GpApiRequest function.
+#' @param model_id optional Character string. The ID of the Google Prediction
+#'   Model, where applicable to the request.
+#' @param method optional Character string. The Google Predication API method to
+#'   perform, where not implied by req_type.
+#' @param model_type optional Character string. The type of model to use, only
+#'   applies to request to perform a prediction. Possible values include
 GpApiRequest <- function(req_type, oauth, project, body_list = NULL, model_id = NULL, method = NULL, model_type = "trainedmodels") {
   stopifnot(
     req_type %in% c("GET", "POST", "PUT", "DELETE")
